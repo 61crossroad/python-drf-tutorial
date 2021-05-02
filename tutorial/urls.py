c7a51b1c411789f1791cfa258e14b7acd3735403
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-03 00:19:21 +0900

    serializer CRUD

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -26,5 +26,6 @@ router.register(r'group', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls')),
 ]
