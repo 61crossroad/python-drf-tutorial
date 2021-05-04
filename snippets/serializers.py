3711129a1d4608f4252cf87980f95a87f1d07961
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-05 03:12:46 +0900

    Relationships & Hyperlinked APis...

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,15 +5,18 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style'
+                  , 'owner', 'url', 'highlight']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    # snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['id', 'username', 'snippets', 'url']
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,19 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
+# from rest_framework import routers
 
-import quickstart.views
+# import quickstart.views
 # from quickstart import views
 
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
 # router.register(r'users', views.UserViewSet)
 # router.register(r'group', views.GroupViewSet)
-router.register(r'group', quickstart.views.GroupViewSet)
+# router.register(r'group', quickstart.views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('', include('snippets.urls')),
 ]
